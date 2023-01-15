6bdc9d4ce004b4740575e9ca35a5dcce47a60f93
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-01-15 19:29:47 +0100

    s

--- SubnauticaSync.bat
+++ SubnauticaSync.bat
@@ -7,7 +7,7 @@ set commit_msg=
     ) else (
         set "commit_msg=%commit_msg%"
         cd C:/Users/mathi/AppData/LocalLow/Unknown Worlds/Subnautica/Subnautica/SavedGames && git add . && git commit -m "%commit_msg%" && git pull --rebase --set-upstream https://github.com/Malachite01/subnauticaSaves.git master && git pull --set-upstream https://github.com/Malachite01/subnauticaSaves.git master && git push --set-upstream https://github.com/Malachite01/subnauticaSaves.git master
-        echo MsgBox "Git push effectue", vbYesOnly, "Notification" > notification.vbs
+        echo MsgBox "Git sync effectue", vbYesOnly, "Notification" > notification.vbs
         cscript notification.vbs
         del notification.vbs
         goto :eof
