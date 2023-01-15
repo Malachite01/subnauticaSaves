fa332875947b41b65103cbda1a167d68168199cf
Mathieu Antunes <112857106+Malachite01@users.noreply.github.com>
2023-01-15 19:01:48 +0100

    Delete SubnauticaPULL.bat

--- SubnauticaPULL.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd C:/Users/mathi/AppData/LocalLow/Unknown Worlds/Subnautica/Subnautica/SavedGames && git pull --set-upstream https://github.com/Malachite01/subnauticaSaves.git master
-echo MsgBox "Git pull effectue", vbYesOnly, "Notification" > notification.vbs
-cscript notification.vbs
-del notification.vbs
-goto :eof
