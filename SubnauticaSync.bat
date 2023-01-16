37da1a8cdcb7a7ced3b0f96007aeb7117abfbaea
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-01-16 09:39:34 +0100

    ajout variables

--- SubnauticaSync.bat
+++ SubnauticaSync.bat
@@ -1,4 +1,5 @@
 @echo off
+set repositoryURL=https://github.com/Malachite01/subnauticaSaves.git
 set commit_msg=
 :loop
     set /p commit_msg=Entres le message de commit, ou es tu dans le jeu ? :
@@ -6,7 +7,7 @@ set commit_msg=
         echo Veuillez saisir un message de commit
     ) else (
         set "commit_msg=%commit_msg%"
-        cd C:\Users\%USERNAME%\AppData\LocalLow\Unknown Worlds\Subnautica\Subnautica\SavedGames && git stash save "%commit_msg%" && git pull --rebase --set-upstream https://github.com/Malachite01/subnauticaSaves.git master && git stash apply && git add . && git commit -m "%commit_msg%" && git push --set-upstream https://github.com/Malachite01/subnauticaSaves.git master
+        cd C:\Users\%USERNAME%\AppData\LocalLow\Unknown Worlds\Subnautica\Subnautica\SavedGames && git stash save "%commit_msg%" && git pull --rebase --set-upstream %repositoryURL% master && git stash apply && git add . && git commit -m "%commit_msg%" && git push --set-upstream %repositoryURL% master
         echo MsgBox "Git sync effectue", vbYesOnly, "Notification" > notification.vbs
         cscript notification.vbs
         del notification.vbs
