c1c9287c034b4c101665a75ad0ef55ff29b7df6c
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-01-16 09:40:55 +0100

    Ajout de variables de path

--- SubnauticaSync.bat
+++ SubnauticaSync.bat
@@ -1,13 +1,14 @@
 @echo off
+set savePATH=C:\Users\%USERNAME%\AppData\LocalLow\Unknown Worlds\Subnautica\Subnautica\SavedGames
 set repositoryURL=https://github.com/Malachite01/subnauticaSaves.git
 set commit_msg=
 :loop
-    set /p commit_msg=Entres le message de commit, ou es tu dans le jeu ? :
+    set /p commit_msg=Entres le message de commit, ou es tu dans le jeu ? : 
     if not defined commit_msg (
         echo Veuillez saisir un message de commit
     ) else (
         set "commit_msg=%commit_msg%"
-        cd C:\Users\%USERNAME%\AppData\LocalLow\Unknown Worlds\Subnautica\Subnautica\SavedGames && git stash save "%commit_msg%" && git pull --rebase --set-upstream %repositoryURL% master && git stash apply && git add . && git commit -m "%commit_msg%" && git push --set-upstream %repositoryURL% master
+        cd %savePATH% && git stash save "%commit_msg%" && git pull --rebase --set-upstream %repositoryURL% master && git stash apply && git add . && git commit -m "%commit_msg%" && git push --set-upstream %repositoryURL% master
         echo MsgBox "Git sync effectue", vbYesOnly, "Notification" > notification.vbs
         cscript notification.vbs
         del notification.vbs
