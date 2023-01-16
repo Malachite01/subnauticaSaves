6cc5a7a307926a760e0585399a54a9872b1e9602
Mathieu Antunes <mathieu.antunes31@gmail.com>
2023-01-16 08:53:09 +0100

    Modification du path

--- SubnauticaSync.bat
+++ SubnauticaSync.bat
@@ -6,7 +6,7 @@ set commit_msg=
         echo Veuillez saisir un message de commit
     ) else (
         set "commit_msg=%commit_msg%"
-        cd C:/Users/mathi/AppData/LocalLow/Unknown Worlds/Subnautica/Subnautica/SavedGames && git stash save "%commit_msg%" && git pull --rebase --set-upstream https://github.com/Malachite01/subnauticaSaves.git master && git stash apply && git add . && git commit -m "%commit_msg%" && git push --set-upstream https://github.com/Malachite01/subnauticaSaves.git master
+        cd C:\Users\%USERNAME%\AppData\LocalLow\Unknown Worlds\Subnautica\Subnautica\SavedGames && git stash save "%commit_msg%" && git pull --rebase --set-upstream https://github.com/Malachite01/subnauticaSaves.git master && git stash apply && git add . && git commit -m "%commit_msg%" && git push --set-upstream https://github.com/Malachite01/subnauticaSaves.git master
         echo MsgBox "Git sync effectue", vbYesOnly, "Notification" > notification.vbs
         cscript notification.vbs
         del notification.vbs
